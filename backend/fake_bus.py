e2cc0ad3e662fbe7177ff4ecb93feb3ed3b0627b
Irina Kasatkina <kasatkina235@gmail.com>
2023-09-22 07:26:32 +0300

    Send data to browser

--- backend/fake_bus.py
+++ backend/fake_bus.py
@@ -9,7 +9,7 @@ from trio_websocket import open_websocket_url
 
 def load_routes(directory_path='routes'):
     for filename in os.listdir(directory_path):
-        if filename.endswith(".json"):
+        if filename.endswith('.json'):
             filepath = os.path.join(directory_path, filename)
             with open(filepath, 'r', encoding='utf8') as file:
                 yield json.load(file)
@@ -20,9 +20,9 @@ async def run_bus(url, bus_id, route):
         try:
             async with open_websocket_url(url) as ws:
                 for coordinate in route['coordinates']:
-                    bus = {'busId': bus_id, 'lat': coordinate[0], 'lng': coordinate[1], 'route': bus_id}
+                    bus = {bus_id: {'busId': bus_id, 'lat': coordinate[0], 'lng': coordinate[1], 'route': bus_id}}
                     await ws.send_message(json.dumps(bus))
-                    await trio.sleep(1)
+                    await trio.sleep(2)
         except  OSError as ose:
             print('Connection attempt failed: %s' % ose, file=stderr)
             break
--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,45 @@
 import json
-from pathlib import Path
+import logging
 
 import trio
-from trio_websocket import serve_websocket, ConnectionClosed
+from trio_websocket import ConnectionClosed, serve_websocket
+
+
+buses = {}
 
 
 async def get_messages(request):
     ws = await request.accept()
     while True:
         try:
-            message = await ws.get_message()
-            print(message)
+            bus_id, bus_info = json.loads(await ws.get_message()).popitem()
+            logging.warning(f'get_messages(): bus_id={bus_id}, bus_info={bus_info}')
+            buses[bus_id] = bus_info
             await trio.sleep(1)
         except ConnectionClosed:
             break
 
 
+async def talk_to_browser(request):
+    global buses
+    ws = await request.accept()
+    while True:
+        try:
+            response = json.dumps({
+                'msgType': 'Buses',
+                'buses': [buses[bus_id] for bus_id in buses],
+            })
+            logging.warning(f'talk_to_browser(): response={response}')
+            await ws.send_message(response)
+            await trio.sleep(1)
+        except ConnectionClosed:
+            break    
+
+
 async def main():
-    await serve_websocket(get_messages, '127.0.0.1', 8080, ssl_context=None)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(serve_websocket, get_messages, '127.0.0.1', 8080, None)
+        nursery.start_soon(serve_websocket, talk_to_browser, '127.0.0.1', 8000, None)
 
 
 if __name__ == '__main__':
